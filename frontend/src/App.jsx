3194cf9cfbefbee3d425b34c15e1cdf6f484131f
Prachi290-pr <prachisawant38079@gmail.com>
2025-07-08 03:10:34 +0530

    first commit : App Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-yellow-400">
+      Responsive Box
+    </div>
+
+
+
+
+  )
+}
+
+export default App
